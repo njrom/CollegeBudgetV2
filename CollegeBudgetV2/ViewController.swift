697c61e4af5b5b8ff5b76a9649741c8772505c43
Nicholas Romano <njrom@nicholass-macbook-pro.local>
2018-12-23 20:52:21 -0500

    Initial Commit

---
+++ CollegeBudgetV2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollegeBudgetV2
+//
+//  Created by Nicholas Romano on 12/23/18.
+//  Copyright © 2018 Nicholas Romano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
