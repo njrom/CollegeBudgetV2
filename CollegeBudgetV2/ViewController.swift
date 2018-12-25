0bb4baf41e4ba4fa4aa7ee7a746c0fbe4862f67d
Nicholas Romano <njrom@nicholass-macbook-pro.local>
2018-12-25 14:33:45 -0500

    Budgets core data saving and loading setup

--- CollegeBudgetV2/ViewController.swift
+++ CollegeBudgetV2/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var budgets = [Budget]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadModel()
+        
+    }
+    
+    func loadModel() {
+        let request : NSFetchRequest<Budget> = Budget.fetchRequest()
+        
+        do {
+            budgets = try context.fetch(request)
+        } catch {
+            print("Error loading categories \(error)")
+        }
+        
+        tablview.reloadData()
+        
+    }
+    
+    func saveModel() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving category \(error)")
+        }
+        
+        tableView.reloadData()
+        
     }
 
 
