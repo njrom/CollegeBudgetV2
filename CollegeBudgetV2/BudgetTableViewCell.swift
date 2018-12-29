b6a480737ccfbfffb334fbb8f033d1dd47cf54f7
Nicholas Romano <njrom@nicholass-macbook-pro.local>
2018-12-29 18:45:49 -0500

    Added Storyboard elements for budget and Transactions Screen

---
+++ CollegeBudgetV2/BudgetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BudgetTableViewCell.swift
+//  CollegeBudgetV2
+//
+//  Created by Nicholas Romano on 12/29/18.
+//  Copyright © 2018 Nicholas Romano. All rights reserved.
+//
+
+import UIKit
+
+class BudgetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
